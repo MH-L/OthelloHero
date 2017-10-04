9d3ed9ba7415681085e3df4ad3c670aa6ceb7906
mh-l <922lmh922@gmail.com>
2017-10-03 18:48:16 -0700

    First step to building a super AI!

---
+++ src/model/OthelloBoard.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.List;
+
+public class OthelloBoard {
+    public static final int WIDTH = 8;
+    public static final int HEIGHT = 8;
+    private short[] rows;
+    private short[] cols;
+    private short[] ltorDiags;
+    private short[] rtolDiags;
+    private List<Byte> mobility;
+    private
+
+}
