c8dadc790b22df9781c07c90d8fa1ba885db7e7d
mh-l <922lmh922@gmail.com>
2017-10-04 00:44:29 -0700

    Added some more functions.

--- src/model/OthelloBoard.java
+++ src/model/OthelloBoard.java
@@ -8,20 +8,22 @@ import java.util.Map;
 public class OthelloBoard {
     public static final int WIDTH = 8;
     public static final int HEIGHT = 8;
-    private static final int BLACK_PIECE = 1;
-    private static final int WHITE_PIECE = 2;
+    private static final short BLACK_PIECE = 1;
+    private static final short WHITE_PIECE = 2;
+    private static final short NO_PIECE = 0;
     private short[] rows;
     private short[] cols;
     private short[] ltorDiags;
     private short[] rtolDiags;
     private List<Byte> mobility;
+    private int totalPieces = 0;
 
     // Stores which positions are valid next moves for each line for white
-    // The first 8 bits of value (most significant) store flip pattern, and the last 8 bits store validity of positions
-    private static List<Map<Short, Short>> flipMapWhite;
+    // The key of value (most significant) store valid locations, and its corresponding key is the flip pattern
+    private static List<Map<Short, Map<Integer, Short>>> flipMapWhite;
 
     // Stores which positions are valid next moves for each line for black
-    private static List<Map<Short, Short>> flipMapBlack;
+    private static List<Map<Short, Map<Integer, Short>>> flipMapBlack;
 
     private static short[] flipPatternToAdder;
 
@@ -54,13 +56,121 @@ public class OthelloBoard {
     }
 
     private static void calculateFlipMap() {
+        // TODO how to enumerate all possibilities?
+        for (int len = 8; len >= 3; len--) {
+            for (int i = 0; i < Math.pow(3, len); i++) {
+                String thirdString = Integer.toString(i, 3);
+                short curLine = Short.parseShort(thirdString, 4);
+                Map<Integer, Short> retVal = findFlipSequence(curLine, len, true);
+                Map<Integer, Short> retVal2 = findFlipSequence(curLine, len, false);
+                if (!retVal.isEmpty())
+                    flipMapBlack.get(len).put(curLine, retVal);
+                if (!retVal2.isEmpty())
+                    flipMapWhite.get(len).put(curLine, retVal2);
 
+            }
+        }
     }
 
+    private static Map<Integer, Short> findFlipSequence(short line, int length, boolean isFirst) {
+        short selfPiece = isFirst ? BLACK_PIECE : WHITE_PIECE;
+        short oppPiece = isFirst ? WHITE_PIECE : BLACK_PIECE;
+        Map<Integer, Short> returnVal = new HashMap<>();
+        for (int i = 0; i < length; i++) {
+            short currentPiece = (short) ((line << (14 - i*2)) >> 14);
+            if (currentPiece != NO_PIECE)
+                continue;
+            for (int j = i + 1; j < length; j++) {
+                short curPiece = (short) ((line << (14 - j*2)) >> 14);
+                if (curPiece == selfPiece && j > i + 1) {
+                    short flipper = 0;
+                    for (int k = i + 1; k < j; k++) {
+                        flipper += (1 << (k*2));
+                    }
+                    returnVal.put(i, flipper);
+                    break;
+                } else if (curPiece == NO_PIECE)
+                    break;
+            }
+        }
+
+        return returnVal;
+    }
+
+    /**
+     * Constructor
+     */
     public OthelloBoard() {
         rows = new short[HEIGHT];
         cols = new short[WIDTH];
-        ltorDiags = new short[WIDTH];
-        rtolDiags = new short[HEIGHT];
+        ltorDiags = new short[HEIGHT + WIDTH - 1];
+        rtolDiags = new short[HEIGHT + WIDTH - 1];
+        mobility = new ArrayList<>();
+    }
+
+    public boolean updateBoard(int move, boolean isFirst) {
+        short stone = isFirst ? BLACK_PIECE : WHITE_PIECE;
+        if (totalPieces < 4) {
+            // During initial setup phase
+            int rowIndex = move / WIDTH;
+            int colIndex = move % WIDTH;
+            rows[rowIndex] |= (stone << (colIndex * 2));
+            cols[colIndex] |= (stone << (rowIndex * 2));
+            int ltorIndex = getltorDiagIndex(move);
+            int rtolIndex = getrtolDiagIndex(move);
+        }
+
+        // TODO
+        return false;
+    }
+
+    private int getltorDiagIndex(int position) {
+        int rowIndex = position / WIDTH;
+        int colIndex = position % WIDTH;
+        return rowIndex - colIndex + WIDTH - 1;
+    }
+
+    private int getrtolDiagIndex(int position) {
+        int rowIndex = position / WIDTH;
+        int colIndex = position % WIDTH;
+        return rowIndex + colIndex;
+    }
+
+    private int getIndexOnLtoR(int position) {
+        int rowIndex = position / WIDTH;
+        int colIndex = position % WIDTH;
+        int ltorIdx = getltorDiagIndex(position);
+        return ltorIdx < WIDTH ? rowIndex : colIndex;
+    }
+
+    private int getIndexOnRtoL(int position) {
+        int rowIndex = position / WIDTH;
+        int colIndex = position % WIDTH;
+        int rtolIdx = getrtolDiagIndex(position);
+        return rtolIdx < WIDTH ? rowIndex : WIDTH - 1 - colIndex;
+    }
+
+    private int lrDiagToBoardPosition(int lrIndex, int indexOnLR) {
+        if (lrIndex < WIDTH) {
+            int rowIndex = indexOnLR;
+            int colIndex = (WIDTH - 1 - lrIndex) + indexOnLR;
+            return rowIndex * WIDTH + colIndex;
+        } else {
+            int rowIndex = (lrIndex - WIDTH + 1) + indexOnLR;
+            int colIndex = indexOnLR;
+            return rowIndex * WIDTH + colIndex;
+        }
+    }
+
+    private int rlDiagToBoardPosition(int rlIndex, int indexOnRL) {
+        if (rlIndex < WIDTH) {
+            int rowIndex = indexOnRL;
+            int colIndex = rlIndex - indexOnRL;
+            return rowIndex * WIDTH + colIndex;
+        } else {
+            int rowIndex = (rlIndex - WIDTH + 1) + indexOnRL;
+            int colIndex = WIDTH - 1 - indexOnRL;
+            return rowIndex * WIDTH + colIndex;
+        }
     }
 }
